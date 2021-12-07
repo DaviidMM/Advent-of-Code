dce73a6b526b9c2981ef604a63e392bfce184572
David Mulero <davicitoo1612@gmail.com>
2021-12-07 17:57:52 +0100

    Añadir solución a la parte 2 del reto 2

---
+++ Day 2 - Dive!/parte_2.js
@@ -0,0 +1,23 @@
+import {input, testInput} from './input.js'
+
+const position = {
+  horizontal: 0,
+  depth: 0,
+  aim: 0
+}
+
+input.forEach(command => {
+  const movement = {
+    direction: command.split(" ")[0],
+    units: parseInt(command.split(" ")[1])
+  }
+  if(movement.direction === "forward"){
+    position.horizontal+=movement.units
+    position.depth+=movement.units*position.aim
+  } else if(movement.direction === "up"){
+    position.aim-=movement.units
+  } else if(movement.direction === "down"){
+    position.aim+=movement.units
+  }
+});
+console.log(`Resultado del reto: ${position.horizontal * position.depth}`); // 1845455714
